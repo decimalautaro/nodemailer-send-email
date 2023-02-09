4c5bd5f4ef325d245ba8ca5347c100111f15329c
Lautaro Decima <decimalautaro@gmail.com>
2023-02-09 18:58:52 -0300

    feat: agregando cors

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 const path= require('path');
+const cors = require('cors')
 
 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+app.use(cors());
 
 
 app.use(require('./routes/router'));
 
-app.use(express.static(path.join(__dirname,'public')));
+// app.use(express.static(path.join(__dirname,'public')));
 
 const PORT = process.env.PORT || 8000;
 
